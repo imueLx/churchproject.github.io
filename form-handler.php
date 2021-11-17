9b98de5b2646576eac2e058957616198bcc3d348
John Uel Mangaba <36302966+devUel@users.noreply.github.com>
2021-11-17 23:40:37 +0800

    website@

--- form-handler.php
+++ form-handler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_from = 'info@yourwebsite';
+$email_from = 'contact@churchproject.me';
 
 $email_subject = 'New Form Submission';
 
